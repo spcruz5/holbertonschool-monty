4ca9bf54542d96c7bc12505debb58f972362a7b9
raycast5 <ray.j.colon@gmail.com>
2022-08-19 04:54:16 +0000

    Added error function

--- tests/errors.c
+++ tests/errors.c
@@ -1,50 +1,21 @@
-	if (ac != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	new = malloc(sizeof(stack_t));
-	if (!new)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-	fd = open(av[1], O_RDONLY)
-	if (fd == -1)
+#include "monty.h"
+
+
+void _perror(char *str, int lineno, int errtype)
+{
+	if (errtype == 1)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
-	for (i = 0; flag != EOF; i++)
+	if (errtype == 2)
 	{
-		flag = getline(&lineptr, &n, fd);
-		lineno++;
-		if (flag > 0)
-			purse = tokenize(lineptr);
-		for (j = 0; purse[j]; j++)
-		{
-			ptr = matchf(purse[j]);
-			if (ptr)
-			{
-				if (check == 1)
-				{
-					ares = atoi(purse[j + 1]);
-					if(ares !=  0)
-						ptr(&head, ares);
-					else
-					{
-						fprintf(stderr, "L%d: usage: push integer\n", lineno);
-						exit(EXIT_FAILURE);
-					}
-				}
-				else
-					ptr(&head, lineno);
-			}
-			else
-			{
-				fprintf(stderr, "L%d: unknown instruction\n", lineno);
-				exit(EXIT_FAILURE);
-			}
-		}
+		fprintf(stderr, "L%d: usage: push integer\n", lineno);
+		exit(EXIT_FAILURE);
 	}
-return (0);
+	if (errtype == 3)
+	{
+		fprintf(stderr, "L%d: unknown instruction\n", lineno);
+		exit(EXIT_FAILURE);
+	}
+}
